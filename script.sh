6d3800afbb3f90ece482bfcf1ae8b9f88f77996b
rarorza <rarorza@proton.me>
2022-11-27 18:53:11 -0300

    update

--- script.sh
+++ script.sh
@@ -2,23 +2,24 @@
 
 set -e
 
-pacotes=( awesome rofi picom alacritty xterm ranger htop git vim neovim vlc smplayer lutris steam wine winetricks xf86-input-wacom libwacom xorg-xinput lxappearance qt5ct neofetch nitrogen polkit-gnome ntfs-3g fuse bluez bluez-utils hplip flatpak gimp alsa-utils flameshot qbittorrent firefox piper)
+packages=( awesome rofi picom alacritty xterm ranger htop git vim neovim vlc smplayer lutris steam wine winetricks xf86-input-wacom libwacom xorg-xinput lxappearance qt5ct neofetch nitrogen polkit-gnome ntfs-3g fuse bluez bluez-utils hplip flatpak gimp alsa-utils flameshot qbittorrent firefox piper)
 
-pacotes_yay=( google-chrome winff qt5-styleplugins grub-customizer hplip-plugin )
+packages_yay=( google-chrome winff qt5-styleplugins grub-customizer hplip-plugin )
 
-pacotes_flatpak=( bitwarden discord snes9x pcsx2 rpcs3 citra duckstation ryujinx yuzu )
-
-git clone https://github.com/rarorza/awesome/
-cp -r awesome/ /home/"$USER"/.config/
-cp -r /home/"$USER"/.config/awesome/dotfiles/.scripts/ /home/"$USER"/
-cp -r /home/"$USER"/.config/awesome/dotfiles/alacritty/ /home/"$USER"/.config/
-cp -r /home/"$USER"/.config/awesome/dotfiles/picom/ /home/"$USER"/.config/
-cp -r /home/"$USER"/.config/awesome/dotfiles/ranger/ /home/"$USER"/.config/
-cp -r /home/"$USER"/.config/awesome/dotfiles/rofi/themes/android_notification.rasi /usr/share/rofi/themes/
-cp -r /home/"$USER"/.config/awesome/dotfiles/themes\ awesomewm/Dark/ /usr/share/awesome/themes/
-cp -r /home/"$USER"/.config/awesome/dotfiles/.scripts/rofi-power-menu/rofi-power-menu /usr/bin/
+packages_flatpak=( bitwarden discord snes9x pcsx2 rpcs3 citra duckstation ryujinx yuzu )
 
 pacman -Syu
-pacman -S ${pacotes[@]} --noconfirm
+pacman -S ${pacotes[@]}
+#pacman -S ${pacotes[@]} --noconfirm
+
+git clone https://github.com/rarorza/awesome/
+cp -r awesome/ /home/rarorza/.config/
+cp -r /home/rarorza/.config/awesome/dotfiles/.scripts/ /home/rarorza/
+cp -r /home/rarorza/.config/awesome/dotfiles/alacritty/ /home/rarorza/.config/
+cp -r /home/rarorza/.config/awesome/dotfiles/picom/ /home/rarorza/.config/
+cp -r /home/rarorza/.config/awesome/dotfiles/ranger/ /home/rarorza/.config/
+cp -r /home/rarorza/.config/awesome/dotfiles/rofi/themes/android_notification.rasi /usr/share/rofi/themes/
+cp -r /home/rarorza/.config/awesome/dotfiles/themes\ awesomewm/Dark/ /usr/share/awesome/themes/
+cp -r /home/rarorza/.config/awesome/dotfiles/.scripts/rofi-power-menu/rofi-power-menu /usr/bin/
 
 #continuar = https://plus.diolinux.com.br/t/tutorial-faca-seu-proprio-script-de-pos-instalacao/10021
