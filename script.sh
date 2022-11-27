43861b487e8b42c3b2124e7651db9a30fd44a38a
rarorza <rarorza@proton.me>
2022-11-27 19:07:24 -0300

    update

--- script.sh
+++ script.sh
@@ -9,17 +9,25 @@ packages_yay=( google-chrome winff qt5-styleplugins grub-customizer hplip-plugin
 packages_flatpak=( bitwarden discord snes9x pcsx2 rpcs3 citra duckstation ryujinx yuzu )
 
 pacman -Syu
-pacman -S ${pacotes[@]}
-#pacman -S ${pacotes[@]} --noconfirm
+pacman -S ${packages[@]}
+#pacman -S ${packages[@]} --noconfirm
+
+
 
 git clone https://github.com/rarorza/awesome/
+
 cp -r awesome/ /home/rarorza/.config/
 cp -r /home/rarorza/.config/awesome/dotfiles/.scripts/ /home/rarorza/
 cp -r /home/rarorza/.config/awesome/dotfiles/alacritty/ /home/rarorza/.config/
+cp -r /home/rarorza/.config/awesome/dotfiles/alacritty/.local/share/fonts/ home/rarorza/.local/share/
 cp -r /home/rarorza/.config/awesome/dotfiles/picom/ /home/rarorza/.config/
 cp -r /home/rarorza/.config/awesome/dotfiles/ranger/ /home/rarorza/.config/
 cp -r /home/rarorza/.config/awesome/dotfiles/rofi/themes/android_notification.rasi /usr/share/rofi/themes/
 cp -r /home/rarorza/.config/awesome/dotfiles/themes\ awesomewm/Dark/ /usr/share/awesome/themes/
 cp -r /home/rarorza/.config/awesome/dotfiles/.scripts/rofi-power-menu/rofi-power-menu /usr/bin/
+
+fv-cache -vf
+
+rofi-theme-selector
 
 #continuar = https://plus.diolinux.com.br/t/tutorial-faca-seu-proprio-script-de-pos-instalacao/10021
