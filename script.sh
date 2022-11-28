0717609c4cc3c8273b4a28eca9f43598f70508e3
rarorza <rarorza@proton.me>
2022-11-27 22:39:46 -0300

    Update

--- script.sh
+++ script.sh
@@ -5,14 +5,12 @@ set -e
 packages=( awesome rofi picom alacritty xterm ranger htop vim neovim vlc smplayer lutris wine winetricks xf86-input-wacom libwacom xorg-xinput lxappearance qt5ct neofetch nitrogen polkit-gnome ntfs-3g fuse bluez bluez-utils hplip flatpak gimp alsa-utils flameshot qbittorrent firefox piper calibre nodejs yarn npm rust )
 
 packages_options=( steam )
-#
 
 packages_yay=( google-chrome winff qt5-styleplugins grub-customizer hplip-plugin zsh ttf-meslo-nerd-font-powerlevel10k powerline-fonts awesome-terminal-fonts zsh-theme-powerlevel10k-git )
 
 packages_flatpak=( bitwarden snes9x pcsx2 rpcs3 citra duckstation ryujinx yuzu )
 
 packages_options_flatpak=( discord )
-#
 
 sudo pacman -Syu
 sudo pacman -S ${packages[@]} --noconfirm
@@ -23,10 +21,8 @@ cd yay/
 makepkg -si
 cd ..
 yay -S ${packages_yay[@]} --noconfirm
-#
 
 flatpak install ${packages_flatpak[@]} -y
-#
 flatpak install ${packages_options_flatpak[@]}
 
 git clone https://github.com/rarorza/awesome/
