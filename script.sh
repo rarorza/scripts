fde9394f89470e301b9bc4756a2fea9aa0ae1283
Rafael R Rosa <79066006+rarorza@users.noreply.github.com>
2023-01-17 19:58:07 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -60,6 +60,9 @@ sudo systemctl start NetworkManager-dispatcher.service
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 
+# Enable docker
+sudo systemctl enable --now docker docker.socket containerd  
+
 # Install Vim Plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config/}"/nvim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
