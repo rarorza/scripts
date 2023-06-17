b3d77b567927684291eeac79f35e426c5bbc8209
Rafael R Rosa <79066006+rarorza@users.noreply.github.com>
2023-06-17 19:04:35 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -92,7 +92,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config/}"/nvim/autoload/plug.vim --cre
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install fonts
-cp -r /$HOME/.config/awesome/dotfiles/alacritty/.local/share/fonts/ /$HOME/.local/share/
 fc-cache -vf
 
 # Install addons for ZSH
